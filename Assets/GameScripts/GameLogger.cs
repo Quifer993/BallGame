b25a865982a8f7d15d953a2ae1afd0cbfb0ccce0
Andrew <a.zolotorevskii@g.nsu.ru>
2024-04-02 15:44:40 +0700

    add logger in main menu

---
+++ Assets/GameScripts/GameLogger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Text;
+
+internal class GameLogger : MonoBehaviour
+{
+    string filename = "logfile.txt";
+
+    private void OnEnable()
+    {
+        Application.logMessageReceived += Log;
+    }
+
+    private void OnDisable()
+    {
+        Application.logMessageReceived -= Log;
+    }
+
+    public void Log(string logString, string stackTrace, LogType type)
+    {
+        using(FileStream fs = File.Open(filename, FileMode.Append, FileAccess.Write, FileShare.Read))
+        {
+            var bytes = Encoding.UTF8.GetBytes("[" + System.DateTime.Now + "] " + logString + "\n");
+            fs.Write(bytes, 0, bytes.Length);
+        }
+        
+    }
+}
